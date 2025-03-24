c23d01c238fedccf9ecfcaac3fa9c94e4c32382e
sanjayk <98270181+ksanjay-ji@users.noreply.github.com>
2025-03-24 17:47:48 +0530

    Update zipthelogs.sh
    .

--- zipthelogs.sh
+++ zipthelogs.sh
@@ -2,13 +2,13 @@
 
 set -x
 LOG_DIR="/var/logs/"
-LOG_FILE=="/Users/sanjaykohar/go/src/shellscript/zip.log"
+LOG_FILE=="/Users/xyz/go/src/shellscript/zip.log"
 
 working_func(){
     #check if log directory present or not
     # if [ ! -f "$LOG_FILE" ]; then
     #     echo "No such file or directory .... Creating file $LOG_FILE"
-    #     # touch /Users/sanjaykohar/go/src/shellscript/zip.log
+    #     # touch /Users/xyz/go/src/shellscript/zip.log
     #     exit 1;
     if [ ! -d "$LOG_DIR" ];then
         echo "Directory not present on the server quiting the program" | tee -a $LOG_FILE
@@ -17,7 +17,7 @@ working_func(){
     echo "$(date): Starting searching for file" | tee -a $LOG_FILE
     FILE=$(find $LOG_DIR -type f -name "usermanagerd.log.0")
     # cat $FILE
-    zip -r "/Users/sanjaykohar/go/src/shellscript/user.zip" $FILE
+    zip -r "/Users/xyz/go/src/shellscript/user.zip" $FILE
     echo "$(date): - zip completed" | tee -a $LOG_FILE
 }
 working_func
